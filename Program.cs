0984a0361a03f2d1bd1994b74d9e46c6edfbfe3e
Nghia Le <85425448+lehieunghia02@users.noreply.github.com>
2026-01-04 23:28:15 +0700

    Add support for forwarded headers in the application

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ using QuanLySoTietKiem.Services.Interfaces;
 using Services.Implementations;
 using Services.Interfaces;
 using Amazon.S3;
+using Microsoft.AspNetCore.HttpOverrides;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -190,14 +191,20 @@ builder.Services.AddHttpClient("PayPal", client =>
 // );
 
 var app = builder.Build();
-
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor |
+        ForwardedHeaders.XForwardedProto |
+        ForwardedHeaders.XForwardedHost
+});
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseHttpsRedirection();
+    // app.UseHttpsRedirection();
 }
 else
 {
